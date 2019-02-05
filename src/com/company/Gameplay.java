53ecffc560119bdc98da72e408e9041e7af7e710
smallneul <nastena.moiseeva.01@mail.ru>
2019-02-05 17:56:38 +0300

    lvl one

--- src/com/company/Gameplay.java
+++ src/com/company/Gameplay.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.awt.Graphics2D;
 import java.awt.*;
 
 
@@ -25,7 +26,10 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private int bx = -1;
     private int by = -2;
 
+
+    private MapGenerator map;
     public Gameplay() {
+        map = new MapGenerator(3,7);
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
@@ -34,19 +38,56 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
     }
      public void paint(Graphics g){
-        g.setColor(Color.pink);
+        g.setColor(Color.black);
         g.fillRect(1,1,692,592);
 
-        g.setColor(Color.orange);
+        map.draw((Graphics2D)g);
+
+        g.setColor(Color.yellow);
         g.fillRect(0,0,3,592);
         g.fillRect(0,0,692,3);
         g.fillRect(691,0,3,592);
 
-        g.setColor(Color.black);
+        g.setColor(Color.white);
+        g.setFont(new Font("serif", Font.BOLD, 25));
+        g.drawString(""+sc, 590, 30);
+
+
+
+        g.setColor(Color.green);
         g.fillRect(pla, 550,100,8);
 
-        g.setColor(Color.red);
+        g.setColor(Color.yellow);
         g.fillOval(bpX, bpY,20,20);
+
+        if(tb <=0){
+            play = false;
+            bx = 0;
+            by = 0;
+            g.setColor(Color.yellow);
+            g.setFont (new Font("serif", Font.BOLD,30));
+            g.drawString("WON, score: " + sc , 190, 300);
+
+            g.setFont (new Font("serif", Font.BOLD,20));
+            g.drawString("Press Enter", 230, 350);
+
+
+        }
+
+        if(bpY > 570){
+            play = false;
+            bx = 0;
+            by = 0;
+            g.setColor(Color.yellow);
+            g.setFont (new Font("serif", Font.BOLD,30));
+            g.drawString("GAME OVER, score: " + sc , 190, 300);
+
+            g.setFont (new Font("serif", Font.BOLD,20));
+            g.drawString("Press Enter", 230, 350);
+
+        }
+
+
 
 
 
@@ -61,6 +102,36 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
             if(new Rectangle(bpX, bpY, 20,20).intersects( new Rectangle(pla, 550,100,8))){
                 by = -by;
             }
+
+            for(int i = 0; i < map.map.length; i++){
+                for(int j=0; j< map.map[0].length; j++){
+                    if(map.map[i][j]>0){
+                        int bX = j*map.bw+80;
+                        int bY = i*map.bh+50;
+                        int bw = map.bw;
+                        int bh = map.bh;
+
+                        Rectangle rect = new Rectangle(bX, bY, bw, bh);
+                        Rectangle ballRect = new Rectangle(bpX, bpY, 20,20);
+                        Rectangle brickRect = rect;
+
+                        if(ballRect.intersects(brickRect)){
+                            map.setBrickValue(0,i,j);
+                            tb--;
+                            sc += 5;
+                            if(bpX + 19 <= brickRect.x || bpX + 1 >= brickRect.x + brickRect.width){
+                                bx = -bx;
+                        }else{
+                                by = -by;
+                            }
+                        break;
+
+                        }
+;                    }
+
+                }
+            }
+
             bpX+=bx;
             bpY+=by;
             if(bpX <0){
@@ -99,6 +170,21 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
             } else {
                 moveLeft();
             }
+        }
+        if(e.getKeyCode() == KeyEvent.VK_ENTER){
+            if(!play){
+                play = true;
+                bpX = 120;
+                bpY = 350;
+                bx = -1;
+                by = -2;
+                pla = 310;
+                sc = 0;
+                tb = 21;
+                map = new MapGenerator(3,7);
+                repaint();
+            }
+
         }
     }
     public void moveRight(){
--- src/com/company/MapGenerator.java
+++ src/com/company/MapGenerator.java
@@ -1,11 +1,41 @@
 package com.company;
 
+
+import java.awt.*;
+
 public class MapGenerator {
     public int map[][];
     public int bh;
     public int bw;
     public MapGenerator(int row, int col){
         map = new int[row][col];
-        
+        for(int i = 0; i < map.length; i++){
+           for(int j=0; j < map[0].length; j++){
+               map[i][j] = 1;
+
+           }
+        }
+        bw =  77;
+        bh = 50;
+
+    }
+    public void draw(Graphics2D g) {
+        for(int i = 0; i<map.length; i++){
+            for(int j=0; j<map[0].length; j++){
+               if(map [i][j] >0){
+                   g.setColor((Color.white));
+                   g.fillRect((j * bw) +80, (i * bw) + 50, bw, bh);
+
+                   g.setStroke(new BasicStroke(3));
+                   g.setColor(Color.black);
+                   g.drawRect((j * bw) +80, (i * bw) + 50, bw, bh);
+               }
+
+            }
+        }
+
+    }
+    public void setBrickValue(int value, int row, int col){
+        map[row][col] = value;
     }
 }
